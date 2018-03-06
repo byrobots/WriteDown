b874740f2797aea801a8f2803979f0d1d23d543c
Dan Gates <dan.gates@pentangle.co.uk>
2018-03-06 22:08:08 +0000

    Tweak how the app is set-up.

--- start.php
+++ start.php
@@ -3,24 +3,26 @@
 /**
  * Create the application container.
  */
-$container = new League\Container\Container;
+$writedown = new WriteDown\WriteDown(
+    new League\Container\Container
+);
 
 /**
  * Define core services.
  */
-$container->share('response', Zend\Diactoros\Response::class);
+$writedown->container()->share('response', Zend\Diactoros\Response::class);
 
-$container->share('request', function () {
+$writedown->container()->share('request', function () {
     return Zend\Diactoros\ServerRequestFactory::fromGlobals(
         $_SERVER, $_GET, $_POST, $_COOKIE, $_FILES
     );
 });
 
-$container->share('emitter', Zend\Diactoros\Response\SapiEmitter::class);
+$writedown->container()->share('emitter', Zend\Diactoros\Response\SapiEmitter::class);
 
-$container->share('routes', new League\Route\RouteCollection);
+$writedown->container()->share('router', new League\Route\RouteCollection);
 
 /**
  * Jump back to processing the request.
  */
-return new WriteDown\WriteDown($container);
+return $writedown;
