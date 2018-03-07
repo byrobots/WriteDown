0045eb77ba02f12267e3d63587b8ec91d56e0ad7
Dan Gates <dan.gates@pentangle.co.uk>
2018-03-07 17:06:08 +0000

    Inversion of control.

--- writedown/WriteDown.php
+++ writedown/WriteDown.php
@@ -2,36 +2,7 @@
 
 namespace WriteDown;
 
-use League\Container\Container;
-
 class WriteDown
 {
-    /**
-     * The service container.
-     *
-     * @var League\Container\Container
-     */
-    private $container;
-
-    /**
-     * Set-up WriteDown.
-     *
-     * @param League\Container\Container $container
-     *
-     * @return Void
-     */
-    public function __construct(Container $container)
-    {
-        $this->container = $container;
-    }
-
-    /**
-     * Get WriteDown's services.
-     *
-     * @return League\Container\Container
-     */
-    public function container()
-    {
-        return $this->container;
-    }
+    //
 }
