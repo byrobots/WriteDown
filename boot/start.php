b0fd870b16fd37c9c1c8df38f2753da04692b81a
By Robots <skynet@by-robots.com>
2018-03-08 16:32:47 +0000

    Use Pimple as our container, I prefer the syntax.

--- boot/start.php
+++ boot/start.php
@@ -3,23 +3,31 @@
 /**
  * Create the application container.
  */
-$container = new League\Container\Container;
-$container->add('app', WriteDown\WriteDown::class);
+$container        = new Pimple\Container;
+$container['app'] = function ($c) {
+    return new WriteDown\WriteDown;
+};
 
 /**
  * Define core services.
  */
-$container->add('response', Zend\Diactoros\Response::class);
+$container['response'] = function ($c) {
+    return new Zend\Diactoros\Response;
+};
 
-$container->add('request', function () {
+$container['request'] = function ($c) {
     return Zend\Diactoros\ServerRequestFactory::fromGlobals(
         $_SERVER, $_GET, $_POST, $_COOKIE, $_FILES
     );
-});
+};
 
-$container->add('emitter', Zend\Diactoros\Response\SapiEmitter::class);
+$container['emitter'] = function ($c) {
+    return new Zend\Diactoros\Response\SapiEmitter;
+};
 
-$container->add('router', new League\Route\RouteCollection);
+$container['router'] = function ($c) {
+    return new League\Route\RouteCollection;
+};
 
 /**
  * Jump back to processing the request.
