de1d61ab3c1005837d99b51c0e50d6f86c099bba
By Robots <skynet@by-robots.com>
2018-03-09 16:23:45 +0000

    Re-arrange for more portability.

--- boot/start.php
+++ boot/start.php
@@ -1,4 +1,11 @@
 <?php
 
+// Include WriteDown's dependencies
+require_once __DIR__ . '/../vendor/autoload.php';
+
+// Get the WriteDown object
+$writedown = new WriteDown\WriteDown;
+
+// Set-up bits and bobs
 require_once __DIR__ . '/container.php';
 require_once __DIR__ . '/routes.php';
--- public/index.php
+++ public/index.php
@@ -1,11 +1,5 @@
 <?php
 
-// Include WriteDown's dependencies
-require_once __DIR__ . '/../vendor/autoload.php';
-
-// Get the WriteDown object
-$writedown = new WriteDown\WriteDown;
-
 // Give WriteDown it's morning Espresso
 require_once __DIR__ . '/../boot/start.php';
 
