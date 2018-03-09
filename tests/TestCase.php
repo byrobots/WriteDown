611d3894635a2b5f1184271abc0b35f05696807b
Dan Gates <dan.gates@pentangle.co.uk>
2018-03-09 16:23:45 +0000

    Re-arrange for more portability.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,6 +7,22 @@ use Mockery\Adapter\Phpunit\MockeryTestCase;
 
 abstract class TestCase extends MockeryTestCase
 {
+    /**
+     * Set-up for testing.
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        $writedown = require_once __DIR__ . '/../boot/start.php';
+        return $writedown;
+    }
+
+    /**
+     * Tidy-up after ourselves.
+     *
+     * @return void
+     */
     public function tearDown()
     {
         Mockery::close();
