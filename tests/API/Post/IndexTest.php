0d437e9e3a768ff33a5d41ac0c4325f6e6b5ff84
By Robots <skynet@by-robots.com>
2018-03-14 20:29:54 +0000

    First failing API test.

---
+++ tests/API/Post/IndexTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\API\Post;
+
+use Tests\TestCase;
+
+class IndexTest extends TestCase
+{
+    public function testNoPosts()
+    {
+        // Request posts
+        $result = $this->writedown->api()->index();
+
+        // Check that an empty array is returned
+        $this->assertEquals([], $result);
+    }
+}
