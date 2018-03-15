69dea3bfeb82812cc0565547ae93d370424b92b0
Dan Gates <dan.gates@pentangle.co.uk>
2018-03-15 08:39:53 +0000

    Check multiple posts are retrieved.

--- tests/API/Post/IndexTest.php
+++ tests/API/Post/IndexTest.php
@@ -25,6 +25,20 @@ class IndexTest extends TestCase
 
         // Check that the result contains one entry
         $this->assertEquals(1, count($result));
-        $this->assertEquals($post->id, $result[0]->id);
+        $this->assertEquals($post->id, $result[0]->id); // Double check the ID. I'd be very confused if this failed, but y'know.
+    }
+
+    public function testRetrievesMany()
+    {
+        $postCount = 5;
+        for ($i = 0; $i < $postCount; $i++) {
+            $this->resources->post();
+        }
+
+        // Request the post index
+        $result = $this->writedown->api()->post()->index();
+
+        // Check that the result contains one entry
+        $this->assertEquals($postCount, count($result));
     }
 }
