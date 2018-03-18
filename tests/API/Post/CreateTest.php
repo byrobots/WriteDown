799b77b41e4937c4d03c6cc6ee7c46a081f248c8
Dan Gates <dan.gates@pentangle.co.uk>
2018-03-18 11:34:48 +0000

    Failing post creation test.

---
+++ tests/API/Post/CreateTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\API\Post;
+
+use Faker\Factory;
+use Tests\TestCase;
+use WriteDown\Entities\Post;
+
+class CreateTest extends TestCase
+{
+    public function testCreated()
+    {
+        // Create a post
+        $faker = Factory::create();
+        $post  = $this->writedown->api()->post()->create([
+            'title'      => $faker->sentence,
+            'slug'       => $faker->slug,
+            'body'       => $faker->paragraph,
+            'publish_at' => new \DateTime('now'),
+        ]);
+
+        // Check we have something
+        $this->assertNotNull($post);
+
+        // Now attempt to retrieve it from the database to make sure it's been
+        // stored
+        $result = $this->writedown->database()
+            ->getRepository('WriteDown\Entities\Post')
+            ->findOneBy(['id' => $post->id]);
+
+        // Check it
+        $this->assertEquals($post->id, $result->id);
+    }
+}
