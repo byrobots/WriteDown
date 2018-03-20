0fd15c312500d46d906b28f258d372a9ce9b533d
By Robots <skynet@by-robots.com>
2018-03-20 20:22:29 +0000

    Basic tests for a slug generator.

---
+++ tests/Misc/Slugger.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Tests\Misc;
+
+use Tests\TestCase;
+use WriteDown\Misc\Slugger as Provider;
+
+class Slugger extends TestCase
+{
+    /**
+     * The slugger interface.
+     *
+     * @var \WriteDown\Misc\Slugger
+     */
+    private $slugger;
+
+    /**
+     * Set up an instance of slugger to test.
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->slugger = new Provider;
+    }
+
+    public function testSingleWord()
+    {
+        $word = $this->faker->word;
+        $this->assertEquals($word, $this->slugger->slug($word));
+    }
+
+    public function testTwoWords()
+    {
+        $input    = 'two words';
+        $expected = 'two-words';
+
+        $this->assertEquals($expected, $this->slugger->slug($input));
+    }
+
+    public function testHyphen()
+    {
+        $input = 'two-words';
+        $this->assertEquals($input, $this->slugger->slug($input));
+    }
+
+    public function testPunctuation()
+    {
+        $input    = 'word!@£$%^&*()_-+={[}]:;"\'|\?/>.<,~`';
+        $expected = 'word';
+
+        $this->assertEquals($expected, $this->slugger->slug($input));
+    }
+}
---
+++ writedown/Misc/Slugger.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace WriteDown\Misc;
+
+/**
+ * Takes a string and converts it into an URL friendly slug.
+ */
+class Slugger
+{
+    //
+}
