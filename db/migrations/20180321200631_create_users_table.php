e418bed04b262df83866e15341a8e23be8006a2c
By Robots <skynet@by-robots.com>
2018-03-21 20:12:36 +0000

    Add users table.

---
+++ db/migrations/20180321200631_create_users_table.php
@@ -0,0 +1,18 @@
+<?php
+
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateUsersTable extends AbstractMigration
+{
+    public function change()
+    {
+        $table = $this->table('users');
+        $table->addColumn('email', 'string')
+            ->addColumn('password', 'string')
+            ->addColumn('created_at', 'datetime')
+            ->addColumn('updated_at', 'datetime')
+            ->addIndex(['email'], ['unique' => true])
+            ->create();
+    }
+}
