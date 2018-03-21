74e667e07bed181e58bd26d003d0c338bd667cc7
Dan Gates <dan.gates@pentangle.co.uk>
2018-03-21 12:24:07 +0000

    Put routes in their own folder.

--- boot/start.php
+++ boot/start.php
@@ -5,6 +5,10 @@ $writedown = new WriteDown\WriteDown(new League\Container\Container);
 
 // Set-up bits and bobs
 require __DIR__ . '/container.php';
-require __DIR__ . '/routes.php';
+
+// Load all route files.
+foreach (glob(__DIR__ . '/../routes/*.php') as $filename) {
+    include $filename;
+}
 
 return $writedown;
