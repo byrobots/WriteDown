d6efd7950518cfd2f8535dd057df71966a1fbf91
Dan Gates <dan.gates@pentangle.co.uk>
2018-03-21 08:38:17 +0000

    Failing tests for #4.

--- tests/API/Post/CreateTest.php
+++ tests/API/Post/CreateTest.php
@@ -53,4 +53,15 @@ class CreateTest extends TestCase
         $this->assertFalse($result['success']);
         $this->assertArrayHasKey('body', $result['data']);
     }
+
+    public function testOnlyFillable()
+    {
+        $result = $this->writedown->api()->post()->create([
+            'title'        => $this->faker->sentence,
+            'body'         => $this->faker->paragraph,
+            'not_fillable' => $this->faker->word,
+        ]);
+
+        $this->assertFalse(property_exists($result['data'], 'not_fillable'));
+    }
 }
