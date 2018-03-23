3ea69976d32a2f624218d6676e2cb5de2245ecc0
By Robots <skynet@by-robots.com>
2018-03-23 13:44:44 +0000

    Unique slug checking class.

--- writedown/Misc/UniqueSlug.php
+++ writedown/Misc/UniqueSlug.php
@@ -2,7 +2,39 @@
 
 namespace WriteDown\Misc;
 
+use Doctrine\ORM\EntityManager;
+
 class UniqueSlug
 {
-    //
+    /**
+     * The EntityManager object.
+     *
+     * @var \Doctrine\ORM\EntityManager
+     */
+    private $db;
+
+    /**
+     * Set-up.
+     *
+     * @param \Doctrine\ORM\EntityManager $database
+     *
+     * @return void
+     */
+    public function __construct(EntityManager $database)
+    {
+        $this->db = $database;
+    }
+
+    /**
+     * Check if the given slug is unique.
+     *
+     * @param string $slug
+     *
+     * @return boolean
+     */
+    public function isUnique($slug)
+    {
+        return !$this->db->getRepository('WriteDown\Entities\Post')
+            ->findOneBy(['slug' => $slug]) ? true : false;
+    }
 }
