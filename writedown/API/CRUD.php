d8ff411b4a85d14de2fba8a0eef9c4611e3bae28
Dan Gates <dan.gates@pentangle.co.uk>
2018-03-23 21:50:22 +0000

    Add a CRUD class so all the CRUD code doesn't need to be repeated for users.

---
+++ writedown/API/CRUD.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace WriteDown\API;
+
+use WriteDown\Entities\Post as Entity;
+
+class CRUD implements EndpointInterface
+{
+    /**
+     * The entity repo.
+     *
+     * @var string
+     */
+    protected $entityRepo;
+
+    /**
+     * The entity class name.
+     *
+     * @var string
+     */
+    protected $entity;
+
+    /**
+     * The EntityManager.
+     *
+     * @var \Doctrine\ORM\EntityManager
+     */
+    protected $db;
+
+    /**
+     * Builds API responses.
+     *
+     * @var \WriteDown\API\ResponseBuilder
+     */
+    protected $response;
+
+    /**
+     * Validates data.
+     *
+     * @var \WriteDown\Validator\ValidatorInterface
+     */
+    protected $validator;
+
+    /**
+     * List all entities.
+     *
+     * @return array
+     */
+    public function index()
+    {
+        $entities = $this->db->getRepository($this->entityRepo)->findAll();
+        return $this->response->build($entities);
+    }
+
+    /**
+     * Retrieve a single entity.
+     *
+     * @param int $entityID
+     *
+     * @return array
+     */
+    public function read($entityID)
+    {
+        $entity = $this->db->getRepository($this->entityRepo)->findOneBy(['id' => $entityID]);
+        if (!$entity) {
+            return $this->response->build(['Not found.'], false);
+        }
+
+        return $this->response->build($entity);
+    }
+
+    /**
+     * Create a new entity.
+     *
+     * @param array $attributes
+     *
+     * @return array
+     */
+    public function create(array $attributes)
+    {
+        // Create the entity by looping through the attributes and populating
+        // fillable ones
+        $entity = new $this->entity;
+        foreach ($attributes as $column => $value) {
+            if (in_array($column, $entity->fillable)) {
+                $entity->$column = $value;
+            }
+        }
+
+        // Validate it
+        if (!$this->validator->validate($entity->rules, $entity->validationArray())) {
+            return $this->response->build($this->validator->errors(), false);
+        }
+
+        // Save it
+        $this->db->persist($entity);
+        $this->db->flush();
+        return $this->response->build($entity);
+    }
+
+    /**
+     * Update an entity.
+     *
+     * @param int   $entityID
+     * @param array $attributes
+     *
+     * @return array
+     */
+    public function update($entityID, array $attributes)
+    {
+        $entity = $this->db->getRepository($this->entityRepo)->findOneBy(['id' => $entityID]);
+        if (!$entity) {
+            return $this->response->build(['Not found.'], false);
+        }
+
+        // Populate entity attributes
+        foreach ($attributes as $column => $value) {
+            if (in_array($column, $entity->fillable)) {
+                $entity->$column = $value;
+            }
+        }
+
+        // Commit to the database and continue
+        $this->db->flush();
+        return $this->response->build($entity);
+    }
+
+    /**
+     * Delete an entity.
+     *
+     * @param int $entityID
+     *
+     * @return array
+     */
+    public function delete($entityID)
+    {
+        $entity = $this->db->getRepository($this->entityRepo)->findOneBy(['id' => $entityID]);
+        if (!$entity) {
+            return $this->response->build(['Not found.'], false);
+        }
+
+        $this->db->remove($entity);
+        $this->db->flush();
+        return $this->response->build([]);
+    }
+}
--- writedown/API/Post/Post.php
+++ writedown/API/Post/Post.php
@@ -3,35 +3,15 @@
 namespace WriteDown\API\Post;
 
 use Doctrine\ORM\EntityManager;
+use WriteDown\API\CRUD;
 use WriteDown\API\EndpointInterface;
 use WriteDown\API\ResponseBuilder;
 use WriteDown\Entities\Post as Entity;
 use WriteDown\Slugs\GenerateSlugInterface;
 use WriteDown\Validator\ValidatorInterface;
 
-class Post implements EndpointInterface
+class Post extends CRUD implements EndpointInterface
 {
-    /**
-     * The EntityManager.
-     *
-     * @var \Doctrine\ORM\EntityManager
-     */
-    private $db;
-
-    /**
-     * Builds API responses.
-     *
-     * @var \WriteDown\API\ResponseBuilder
-     */
-    private $response;
-
-    /**
-     * Validates data.
-     *
-     * @var \WriteDown\Validator\ValidatorInterface
-     */
-    private $validator;
-
     /**
      * Checks slugs are unique.
      *
@@ -55,40 +35,15 @@ class Post implements EndpointInterface
         $this->response   = $response;
         $this->validator  = $validator;
         $this->slug       = $generateSlug;
+
+        // Set additional CRUD settings
+        $this->entityRepo = 'WriteDown\Entities\Post';
+        $this->entity     = 'Post';
     }
 
     /**
-     * List all posts.
-     *
-     * @return array
-     */
-    public function index()
-    {
-        $posts = $this->db->getRepository('WriteDown\Entities\Post')->findAll();
-        return $this->response->build($posts);
-    }
-
-    /**
-     * Retrieve a single post.
-     *
-     * @param int $postID
-     *
-     * @return array
-     */
-    public function read($postID)
-    {
-        $post = $this->db->getRepository('WriteDown\Entities\Post')
-            ->findOneBy(['id' => $postID]);
-
-        if (!$post) {
-            return $this->response->build(['Not found.'], false);
-        }
-
-        return $this->response->build($post);
-    }
-
-    /**
-     * Create a new post.
+     * Create a new post. The CRUD::create() method has to be over-ridden in
+     * order to work with slugs.
      *
      * @param array $attributes
      *
@@ -125,55 +80,5 @@ class Post implements EndpointInterface
         $this->db->persist($post);
         $this->db->flush();
         return $this->response->build($post);
-    }
-
-    /**
-     * Update a post.
-     *
-     * @param int   $postID
-     * @param array $attributes
-     *
-     * @return array
-     */
-    public function update($postID, array $attributes)
-    {
-        $post = $this->db->getRepository('WriteDown\Entities\Post')
-            ->findOneBy(['id' => $postID]);
-
-        if (!$post) {
-            return $this->response->build(['Not found.'], false);
-        }
-
-        // Populate entity attributes
-        foreach ($attributes as $column => $value) {
-            if (in_array($column, $post->fillable)) {
-                $post->$column = $value;
-            }
-        }
-
-        // Commit to the database and continue
-        $this->db->flush();
-        return $this->response->build($post);
-    }
-
-    /**
-     * Delete a post.
-     *
-     * @param int $postID
-     *
-     * @return array
-     */
-    public function delete($postID)
-    {
-        $post = $this->db->getRepository('WriteDown\Entities\Post')
-            ->findOneBy(['id' => $postID]);
-
-        if (!$post) {
-            return $this->response->build(['Not found.'], false);
-        }
-
-        $this->db->remove($post);
-        $this->db->flush();
-        return $this->response->build([]);
     }
 }
