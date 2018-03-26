97ba63537d24200f12aa5fed8d771a2aa3873f85
By Robots <skynet@by-robots.com>
2018-03-26 12:40:05 +0100

    Add a token column to the users table. This will be sued for logging a user in.

--- db/migrations/20180321200631_create_users_table.php
+++ db/migrations/20180321200631_create_users_table.php
@@ -10,6 +10,7 @@ class CreateUsersTable extends AbstractMigration
         $table = $this->table('users');
         $table->addColumn('email', 'string')
             ->addColumn('password', 'string')
+            ->addColumn('token', 'string')
             ->addColumn('created_at', 'datetime')
             ->addColumn('updated_at', 'datetime')
             ->addIndex(['email'], ['unique' => true])
