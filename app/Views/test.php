8e90e773b80cda430bba93dcf62219c4fd2ed776
Dan Gates <dan.gates@pentangle.co.uk>
2018-03-31 09:27:20 +0100

    Basic frontend build process.

--- app/Views/test.php
+++ app/Views/test.php
@@ -4,9 +4,16 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <link rel="stylesheet" href="/css/app.css">
+
     <title>It&rsquo;s alive!</title>
 </head>
 <body>
-    <p>Checking the view generator loads.</p>
+<main role="main" class="container">
+    <h1>WriteDown</h1>
+    <p>Testing views.</p>
+</main>
+
+<script src="/js/app.js"></script>
 </body>
 </html>
