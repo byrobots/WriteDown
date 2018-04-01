278095b48290255a0f7445b320447be71c024309
Dan Gates <dan.gates@pentangle.co.uk>
2018-04-01 16:07:09 +0100

    Login form, sans action or CSRF.

---
+++ app/Http/Controllers/Admin/AuthController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use WriteDown\Http\Controller;
+
+class AuthController extends Controller
+{
+    /**
+     * Show the login form.
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function loginForm()
+    {
+        return $this->view->render($this->response, 'admin/auth/login.php', []);
+    }
+}
