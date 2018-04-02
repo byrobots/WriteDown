80d5fe38b09b31f0d915b5292774c54f8a817bec
Dan Gates <dan.gates@pentangle.co.uk>
2018-04-02 17:38:36 +0100

    Tidy up CSRF middleware handling.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,12 +1,8 @@
 <?php
 
-$csrfMiddleware = new \WriteDown\HTTP\Middleware\CSRFMiddleware(
-    $writedown->getContainer()->get('WriteDown\CSRF\CSRFInterface')
-);
-
 $writedown->getRouter()
     ->get('/admin/login', 'Admin\AuthController::loginForm');
 
 $writedown->getRouter()
     ->post('/admin/login', 'Admin\AuthController::validateLogin')
-    ->middleware([$csrfMiddleware, 'validate']);
+    ->middleware($csrfMiddleware);
