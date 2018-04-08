f13cc987aa80aee248e03a88d822d0656435c7a9
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2018-04-08 14:18:37 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- writedown/API/CRUD.php
+++ writedown/API/CRUD.php
@@ -64,7 +64,7 @@ class CRUD implements EndpointInterface
             return $this->response->build(['Not found.'], false);
         }
 
-        return $this->response->build($entity,true, $this->db->getRepository($this->entityRepo));
+        return $this->response->build($entity, true, $this->db->getRepository($this->entityRepo));
     }
 
     /**
