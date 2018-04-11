c947a71816cb489a3231a2b7c9d2928a222c3016
By Robots <skynet@by-robots.com>
2018-04-11 12:59:18 +0100

    Worj on some dashboard styles.

--- app/Views/admin/dashboard/index.php
+++ app/Views/admin/dashboard/index.php
@@ -9,8 +9,17 @@
     <link href="/css/admin.css" rel="stylesheet">
 </head>
 <body>
-    <div class="container">
-        <p>You&rsquo;re logged in.</p>
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow" id="masthead">
+        <a href="https://www.by-robots.com">
+            <img alt="By Robots" src="/img/logo-tiny.png" srcset="/img/logo-tiny@2x.png 2x">
+        </a>
+        <h2 class="my-0 mr-md-auto font-weight-normal">Write Down</h2>
+
+        <nav class="my-2 my-md-0 mr-md-3">
+            <a class="p-2 text-dark" href="#">Posts</a>
+            <a class="p-2 text-dark" href="#">Users</a>
+        </nav>
+        <a class="btn btn-outline-warning" href="#">Logout</a>
     </div>
     <script src="/js/admin.js"></script>
 </body>
