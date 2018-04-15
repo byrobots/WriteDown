949005f72187baf00ecf028a40c0ee6052504240
Dan Gates <dan.gates@pentangle.co.uk>
2018-04-15 21:38:10 +0100

    getFlash wasn't returning. It is now, but doesn't fix the flash session issue I seem to be having.

--- writedown/Sessions/AuraSession.php
+++ writedown/Sessions/AuraSession.php
@@ -7,7 +7,7 @@ use Aura\Session\SessionFactory;
 /**
  * No need to include this class in code coverage for now, Aura Sessions has
  * it's own tests. Plus, we can't really write to sessions in PHPUnit so a
- * stub we'll be used where needed.
+ * stub will be used where needed.
  *
  * @codeCoverageIgnore
  */
@@ -25,7 +25,7 @@ class AuraSession implements SessionInterface
      */
     public function __construct()
     {
-        $sessionFactory = new SessionFactory();
+        $sessionFactory = new SessionFactory;
         $session        = $sessionFactory->newInstance($_COOKIE);
         $this->segment  = $session->getSegment('WriteDown\WriteDown');
     }
@@ -67,7 +67,7 @@ class AuraSession implements SessionInterface
      */
     public function getFlash($key, $alt = null)
     {
-        $this->segment->getFlash($key, $alt);
+        return $this->segment->getFlash($key, $alt);
     }
 
     /**
