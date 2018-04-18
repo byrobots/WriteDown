1f892cef277758276de225a84c1921399d5723dc
Dan Gates <dan.gates@pentangle.co.uk>
2018-04-18 13:05:14 +0100

    Re-organise PostController to make use of the new CRUDController. This will be there's no repeat code when I do the CRUD stuff for users.

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -4,138 +4,38 @@ namespace App\Http\Controllers\Admin;
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
-use WriteDown\Http\Controllers\Controller;
+use WriteDown\API\Interfaces\APIInterface;
 use Zend\Diactoros\Response\RedirectResponse;
 
-class PostController extends Controller
+class PostController extends CRUDController
 {
     /**
-     * List all posts.
-     *
-     * @return \Psr\Http\Message\ResponseInterface
+     * Set-up CRUDController.
      */
-    public function index()
+    public function __construct()
     {
-        return $this->view->render($this->response, 'admin/post/index.php', [
-            'error' => $this->sessions->getFlash('error'),
-            'posts' => $this->api->post()->index(['where' => []]),
-        ]);
+        $this->viewFolder   = 'post';
+        $this->resourcePath = 'posts';
+        $this->endpoint     = 'post';
     }
 
     /**
-     * Show the creation form.
-     *
-     * @return \Psr\Http\Message\ResponseInterface
-     */
-    public function create()
-    {
-        return $this->view->render($this->response, 'admin/post/create.php', [
-            'csrf'   => $this->csrf->get(),
-            'errors' => $this->sessions->getFlash('errors') ? $this->sessions->getFlash('errors') : [],
-            'old'    => $this->sessions->getFlash('old') ? $this->sessions->getFlash('old') : [],
-        ]);
-    }
-
-    /**
-     * Save the new post.
-     *
-     * @return \Psr\Http\Message\ResponseInterface
+     * @inheritDoc
      */
     public function store()
     {
-        $data               = $this->request->getParsedBody();
-        $data['publish_at'] = new \DateTime($data['publish_at']);
-        $result             = $this->api->post()->create($data);
-
-        if ($result['success']) {
-            $this->sessions
-                ->setFlash('success', 'The new post, ' . $result['data']->title . ', has been saved.');
-
-            return new RedirectResponse('/admin/posts');
-        }
-
-        $this->sessions->setFlash('errors', $result['data']);
-        $this->sessions->setFlash('old', $data);
-        return new RedirectResponse('/admin/posts/new');
+        $this->data               = $this->request->getParsedBody();
+        $this->data['publish_at'] = new \DateTime($this->data['publish_at']);
+        return parent::store();
     }
 
     /**
-     * Show the edit form.
-     *
-     * @param \Psr\Http\Message\ServerRequestInterface $request
-     * @param \Psr\Http\Message\ResponseInterface      $response
-     * @param array                                    $args
-     *
-     * @return \Psr\Http\Message\ResponseInterface
-     */
-    public function edit(ServerRequestInterface $request, ResponseInterface $response, array $args)
-    {
-        $post = $this->api->post()->read($args['postID']);
-        if (!$post['success']) {
-            $this->sessions->setFlash('error', 'Sorry, that post was not found.');
-            return new RedirectResponse('/admin/posts');
-        }
-
-        return $this->view->render($this->response, 'admin/post/edit.php', [
-            'csrf'   => $this->csrf->get(),
-            'errors' => $this->sessions->getFlash('errors') ? $this->sessions->getFlash('errors') : [],
-            'old'    => $this->sessions->getFlash('old') ? $this->sessions->getFlash('old') : [],
-            'post'   => $post,
-        ]);
-    }
-
-    /**
-     * Save changes.
-     *
-     * @param \Psr\Http\Message\ServerRequestInterface $request
-     * @param \Psr\Http\Message\ResponseInterface      $response
-     * @param array                                    $args
-     *
-     * @return \Psr\Http\Message\ResponseInterface
+     * @inheritDoc
      */
     public function update(ServerRequestInterface $request, ResponseInterface $response, array $args)
     {
-        $data               = $this->request->getParsedBody();
-        $data['publish_at'] = new \DateTime($data['publish_at']);
-        $result             = $this->api->post()->update($args['postID'], $data);
-
-        if ($result['success']) {
-            $this->sessions
-                ->setFlash('success', 'The post, ' . $result['data']->title . ', has been updated.');
-
-            return new RedirectResponse('/admin/posts');
-        }
-
-        if ($result['data'] == ['Not found.']) {
-            $this->sessions->setFlash('error', 'That post was not found.');
-            return new RedirectResponse('/admin/posts');
-        }
-
-        $this->sessions->setFlash('errors', $result['data']);
-        $this->sessions->setFlash('old', $data);
-        return new RedirectResponse('/admin/posts/' . $post['data']->id);
-    }
-
-    /**
-     * Delete a post.
-     *
-     * @param \Psr\Http\Message\ServerRequestInterface $request
-     * @param \Psr\Http\Message\ResponseInterface      $response
-     * @param array                                    $args
-     *
-     * @return \Psr\Http\Message\ResponseInterface
-     */
-    public function delete(ServerRequestInterface $request, ResponseInterface $response, array $args)
-    {
-        $result = $this->api->post()->delete($args['postID']);
-        if ($result['success']) {
-            $this->sessions
-                ->setFlash('success', 'The post has been deleted.');
-
-            return new RedirectResponse('/admin/posts');
-        }
-
-        $this->sessions->setFlash('error', 'The post doesn&rsquo;t exist.');
-        return new RedirectResponse('/admin/posts/');
+        $this->data               = $this->request->getParsedBody();
+        $this->data['publish_at'] = new \DateTime($this->data['publish_at']);
+        return parent::update($request, $response, $args);
     }
 }
