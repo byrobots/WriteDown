57783da4a26eb08cc571a9321c317cf94578fe58
Dan Gates <dan.gates@pentangle.co.uk>
2018-04-19 13:55:06 +0100

    And that's pagination.

--- app/Views/admin/parts/pagination.php
+++ app/Views/admin/parts/pagination.php
@@ -1,15 +1,24 @@
 <?php if ($resources['meta']['total_pages'] > 1) { ?>
     <nav>
         <ul class="pagination">
-            <?php if ($resources['meta']['current_page'] == 1) { ?>
-                <li class="page-item disabled">
-                    <a class="page-link" href="#" tabindex="-1">Previous</a>
+            <li class="page-item<?= $resources['meta']['current_page'] == 1 ? ' disabled' : '' ?>">
+                <a class="page-link"
+                   href="<?= $resources['meta']['current_page'] == 1 ? '#' : '/admin/posts/' . ($resources['meta']['current_page'] - 1) ?>"
+                   tabindex="-1">Previous</a>
+            </li>
+
+            <?php $index = 0;
+            while ($index < $resources['meta']['total_pages']) { ?>
+                <li class="page-item<?= $index + 1 == $resources['meta']['current_page'] ? ' active' : '' ?>">
+                    <a href="/admin/posts/<?= $index + 1 ?>" class="page-link"><?= $index + 1 ?></a>
                 </li>
-            <?php } else { ?>
-                <li class="page-item">
-                    <a href="/admin/posts/<?= $resources['meta']['current_page'] - 1 ?>">Previous</a>
-                </li>
-            <?php } ?>
+                <?php $index++;
+            } ?>
+
+            <li class="page-item<?= $resources['meta']['current_page'] == $resources['meta']['total_pages'] ? ' disabled' : '' ?>">
+                <a class="page-link"
+                   href="<?= $resources['meta']['current_page'] == $resources['meta']['total_pages'] ? '#' : '/admin/posts/' . ($resources['meta']['current_page'] + 1) ?>" tabindex="-1">Next</a>
+            </li>
         </ul>
     </nav>
 <?php } ?>
