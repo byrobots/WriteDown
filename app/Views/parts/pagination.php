397aff4e9efe173189c4494ff9522acabfafa0d0
Dan Gates <dan.gates@pentangle.co.uk>
2018-04-21 10:11:48 +0100

    Style up the frontend some more.

--- app/Views/parts/pagination.php
+++ app/Views/parts/pagination.php
@@ -1,23 +1,23 @@
 <?php if ($posts['meta']['total_pages'] > 1) { ?>
-    <nav>
+    <nav class="pagination-container">
         <ul class="pagination">
             <li class="page-item<?= $posts['meta']['current_page'] == 1 ? ' disabled' : '' ?>">
                 <a class="page-link"
-                   href="<?= $posts['meta']['current_page'] == 1 ? '#' : '/posts/' . ($posts['meta']['current_page'] - 1) ?>"
+                   href="<?= $posts['meta']['current_page'] == 1 ? '#' : '/' . ($posts['meta']['current_page'] - 1) ?>"
                    tabindex="-1">Previous</a>
             </li>
 
             <?php $index = 0;
             while ($index < $posts['meta']['total_pages']) { ?>
                 <li class="page-item<?= $index + 1 == $posts['meta']['current_page'] ? ' active' : '' ?>">
-                    <a href="/posts/<?= $index + 1 ?>" class="page-link"><?= $index + 1 ?></a>
+                    <a href="/<?= $index + 1 ?>" class="page-link"><?= $index + 1 ?></a>
                 </li>
                 <?php $index++;
             } ?>
 
             <li class="page-item<?= $posts['meta']['current_page'] == $posts['meta']['total_pages'] ? ' disabled' : '' ?>">
                 <a class="page-link"
-                   href="<?= $posts['meta']['current_page'] == $posts['meta']['total_pages'] ? '#' : '/posts/' . ($posts['meta']['current_page'] + 1) ?>" tabindex="-1">Next</a>
+                   href="<?= $posts['meta']['current_page'] == $posts['meta']['total_pages'] ? '#' : '/' . ($posts['meta']['current_page'] + 1) ?>" tabindex="-1">Next</a>
             </li>
         </ul>
     </nav>
