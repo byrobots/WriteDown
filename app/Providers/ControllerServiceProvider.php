eeebda3f041a16031bf012b2fe02148f88ac389f
Dan Gates <dan.gates@pentangle.co.uk>
2018-04-21 14:17:48 +0100

    Start makring the writedown folder sefl-sufficient so it can be split into it's own repo.

--- app/Providers/ControllerServiceProvider.php
+++ app/Providers/ControllerServiceProvider.php
@@ -3,11 +3,9 @@
 namespace App\Providers;
 
 use App\Http\Controllers\Admin\AuthController;
-use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\PostController;
 use App\Http\Controllers\Admin\UserController;
 use League\Container\ServiceProvider\AbstractServiceProvider;
-use WriteDown\Http\Interfaces\ControllerInterface;
 
 class ControllerServiceProvider extends AbstractServiceProvider
 {
@@ -28,18 +26,6 @@ class ControllerServiceProvider extends AbstractServiceProvider
      */
     public function register()
     {
-        // Set various dependencies all controllers have
-        $this->getContainer()->inflector(ControllerInterface::class)
-            ->invokeMethod('setRequest',  ['Psr\Http\Message\RequestInterface'])
-            ->invokeMethod('setResponse', ['Psr\Http\Message\ResponseInterface'])
-            ->invokeMethod('setSessions', ['WriteDown\Sessions\SessionInterface'])
-            ->invokeMethod('setAPI',      ['WriteDown\API\Interfaces\APIInterface'])
-            ->invokeMethod('setView',     ['view']) // TODO: Interface
-            ->invokeMethod('setCSRF',     ['WriteDown\CSRF\CSRFInterface'])
-            ->invokeMethod('setAuth',     ['WriteDown\Auth\Interfaces\AuthInterface'])
-            ->invokeMethod('setMarkdown', ['WriteDown\Markdown\MarkdownInterface']);
-
-        // Now define the controllers themselves.
         // First up, the admin area.
         $this->getContainer()
             ->add('Admin\AuthController', AuthController::class);
