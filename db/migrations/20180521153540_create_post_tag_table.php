a11632aee6219b750d3d635e30b7f321472d7983
By Robots <skynet@by-robots.com>
2018-05-21 16:54:23 +0100

    Fix post_tag migration.

--- db/migrations/20180521153540_create_post_tag_table.php
+++ db/migrations/20180521153540_create_post_tag_table.php
@@ -12,10 +12,12 @@ class CreatePostTagTable extends AbstractMigration
             ['id' => false, 'primary_key' => ['post_id', 'tag_id']]
         );
 
-        $table->addColumn('post_id', 'integer', ['identity' => true, 'signed' => false])
-            ->addColumn('tag_id', 'integer', ['identity' => true, 'signed' => false])
-            ->addForeignKey('post_id', 'posts', 'id', ['delete'=> 'CASCADE', 'update'=> 'NO_ACTION'])
-            ->addForeignKey('tag_id', 'tags', 'id', ['delete'=> 'CASCADE', 'update'=> 'NO_ACTION'])
+        $table->addColumn('post_id', 'integer')
+            ->addColumn('tag_id', 'integer')
+            ->addColumn('created_at', 'datetime')
+            ->addColumn('updated_at', 'datetime')
+            ->addForeignKey('post_id', 'posts', 'id', ['delete' => 'CASCADE', 'update' => 'NO_ACTION'])
+            ->addForeignKey('tag_id', 'tags', 'id', ['delete' => 'CASCADE', 'update' => 'NO_ACTION'])
             ->create();
     }
 }
