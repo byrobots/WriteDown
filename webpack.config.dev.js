0fe0c8ad277ec9ca05ab2cc6e37832a477da042d
By Robots <skynet@by-robots.com>
2018-10-03 13:46:41 +0100

    Add barebones style.

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -11,6 +11,13 @@ module.exports = {
         }, {
             test: /\.js$/,
             use: 'babel-loader'
+        }, {
+            test: /\.s[c|a]ss$/,
+            use: [
+                "style-loader", // Creates style nodes from JS strings
+                "css-loader",   // Translates CSS into CommonJS
+                "sass-loader"   // Compiles Sass to CSS, using Node Sass by default
+            ]
         }]
     },
     output: {
