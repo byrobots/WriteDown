87644a1660cb21a25fc1dcad9f010fd2202c1339
By Robots <skynet@by-robots.com>
2018-10-05 17:08:47 +0100

    Add the login interaction.

--- src/components/loginform/template.vue
+++ src/components/loginform/template.vue
@@ -1,24 +1,45 @@
 <template>
-    <form method="post" action="" class="login-form">
-        <div class="form-row">
-            <label for="email">Email</label>
-            <input type="email" name="email" id="email">
-        </div>
+    <section id="login-form-container">
+        <form method="post" action="" id="login-form" v-if="false === showSpinner">
+            <div class="form-row">
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email">
+            </div>
 
-        <div class="form-row">
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password">
-        </div>
+            <div class="form-row">
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password">
+            </div>
 
-        <div class="form-row">
-            <button class="submit-button">
-                <i class="fas fa-unlock-alt"></i>
-                Login
-            </button>
-        </div>
-    </form>
+            <div class="form-row">
+                <button class="submit-button" v-on:click="attemptLogin">
+                    <i class="fas fa-unlock-alt"></i>
+                    Login
+                </button>
+            </div>
+        </form>
+
+        <spinner v-if="true === showSpinner" />
+    </section>
 </template>
 
 <script>
-    export default { };
+    import spinner from '../spinner';
+
+    export default {
+        components: { spinner },
+        data () {
+            return {
+                showSpinner: false,
+            };
+        },
+        methods: {
+            attemptLogin (event) {
+                event.preventDefault();
+                this.showSpinner = true;
+
+                // TODO: Attempt to login with the details
+            }
+        },
+    };
 </script>
