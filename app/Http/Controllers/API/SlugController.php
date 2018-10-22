31437bc3e5a18d5ba54d80ea6206c1dcd321ce25
By Robots <skynet@by-robots.com>
2018-10-22 17:07:48 +0100

    Add the SlugController ready to role later.

---
+++ app/Http/Controllers/API/SlugController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+class SlugController extends BaseController
+{
+    /**
+     * Takes a title for a post and calculates the predicted slug for it. Does
+     * not reserve the slug.
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function predicted()
+    {
+        // TODO
+    }
+}
