702909bdc3d77814b57ac828df10f197d181853c
Dan Gates <skynet@by-robots.com>
2019-05-05 14:03:45 +0100

    Tweak how Twig is added to the container so I can extend it later.

--- boot/container.php
+++ boot/container.php
@@ -6,7 +6,7 @@ $container = $writedown->getContainer();
 // Define additional services.
 $container->addServiceProvider(\App\Providers\AdminControllerServiceProvider::class);
 $container->addServiceProvider(\App\Providers\APIControllerServiceProvider::class);
-$container->add('view', new Twig_Environment(
-    new Twig_Loader_Filesystem(__DIR__ . '/../app/Views'),
-    []
-));
+
+// Set-up Twig
+$twig = new Twig_Environment(new Twig_Loader_Filesystem(__DIR__ . '/../app/Views'), []);
+$container->add('view', $twig);
