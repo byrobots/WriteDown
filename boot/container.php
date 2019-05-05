440f0b7f10bd8b7ba8aedafe69ed8bb265c7e269
Dan Gates <skynet@by-robots.com>
2019-05-05 15:01:14 +0100

    Use my forked version of the twig webpack manifest extension.

--- boot/container.php
+++ boot/container.php
@@ -9,4 +9,14 @@ $container->addServiceProvider(\App\Providers\APIControllerServiceProvider::clas
 
 // Set-up Twig
 $twig = new Twig_Environment(new Twig_Loader_Filesystem(__DIR__ . '/../app/Views'), []);
+
+switch (getenv('ENVIRONMENT')) {
+    case 'development':
+        $manifestFile = 'http://localhost:8080/dist/manifest.json';
+        break;
+    default:
+        $manifestFile = __DIR__ . '/../public/dist/manifest.json';
+}
+
+$twig->addExtension(new \ByRobots\TwigWebpackManifestExtension\WebpackExtension($manifestFile));
 $container->add('view', $twig);
